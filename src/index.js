80321dbaa3f16531b992fe5d1f9e97ff040d4f0b
hassan-abdi269 <honze2030@gamail.com>
2025-03-17 08:50:50 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+document.addEventListener("DOMContentLoaded", () => {
+  
+  const form = document.getElementById("create-task-form");
+  const taskList = document.getElementById("tasks");
+
+  
+  form.addEventListener("submit", (e) => {
+    e.preventDefault(); 
+
+    
+    const taskInput = document.getElementById("new-task-description");
+    const taskText = taskInput.value.trim();
+
+    if (taskText === "") return; 
+
+  
+    const taskItem = document.createElement("li");
+    taskItem.textContent = taskText;
+
+    
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "❌";
+    deleteBtn.style.marginLeft = "10px";
+    deleteBtn.addEventListener("click", () => taskItem.remove());
+
+    
+    taskItem.appendChild(deleteBtn);
+    taskList.appendChild(taskItem);
+
+    
+    taskInput.value = "";
+  });
+});
